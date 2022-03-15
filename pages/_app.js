b790792ab7750335861631e2ecbfaedd840c11e1
zoki <zoki_ei@yahoo.com>
2022-03-15 12:37:16 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import React, { useState, useEffect } from 'react';
+
+// import 'tailwindcss/tailwind.css';
+import '../styles/globals.scss'
+import { Layout } from '../components'
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+   return (
+      <Layout>
+         <Component {...pageProps} />
+      </Layout>
+   )
 }
 
 export default MyApp
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+   content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+   ],
+   theme: {
+      extend: {
+         spacing: {
+            '2/3': '66.666667%',
+         },
+      },
+   },
+   plugins: [],
+}
